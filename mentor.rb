10487dd8a43a42fd610a3950b777c95981149db6
Juna.S <js17.nrober@gmail.com>
2021-10-08 15:57:30 +0000

    first commit

---
+++ mentor.rb
@@ -0,0 +1,27 @@
+#Mentorクラスの定義
+class Mentor
+  attr_accessor :name
+  
+  def initialize(name)
+    self.name = name
+  end
+  
+  def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+
+#RailsMentorクラスの定義
+class RailsMentor < Mentor
+  def job
+    puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。" 
+  end
+end
+
+#インスタンスの生成
+kirameki = Mentor.new('煌木')
+akaide = RailsMentor.new('赤出')
+
+#jobの呼び出し
+kirameki.job
+akaide.job
